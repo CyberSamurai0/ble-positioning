ba784bf68c27c6c062469ff6ea73d10041930447
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-11-22 18:01:40 -0500

    Add ANSI color code wrapper for readability

---
+++ handheld/tty_color.py
@@ -0,0 +1,20 @@
+def red(string):
+    return f"\x1b[31m{string}\x1b[0m"
+
+def green(string):
+    return f"\x1b[32m{string}\x1b[0m"
+
+def yellow(string):
+    return f"\x1b[33m{string}\x1b[0m"
+
+def blue(string):
+    return f"\x1b[34m{string}\x1b[0m"
+
+def magenta(string):
+    return f"\x1b[35m{string}\x1b[0m"
+
+def cyan(string):
+    return f"\x1b[36m{string}\x1b[0m"
+
+def gray(string):
+    return f"\x1b[37m{string}\x1b[0m"
