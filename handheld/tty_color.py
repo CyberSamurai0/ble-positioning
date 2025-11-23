c48a50091d08c96be46d6a4ca1c6a04991ca229d
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-11-23 17:12:28 -0500

    Improve readability

--- handheld/tty_color.py
+++ handheld/tty_color.py
@@ -1,20 +1,29 @@
+ANSI_RED = "\x1b[31m"
+ANSI_GREEN = "\x1b[32m"
+ANSI_YELLOW = "\x1b[33m"
+ANSI_BLUE = "\x1b[34m"
+ANSI_MAGENTA = "\x1b[35m"
+ANSI_CYAN = "\x1b[36m"
+ANSI_GRAY = "\x1b[37m"
+ANSI_RESET = "\x1b[0m"
+
 def red(string):
-    return f"\x1b[31m{string}\x1b[0m"
+    return f"{ANSI_RED}{string}{ANSI_RESET}"
 
 def green(string):
-    return f"\x1b[32m{string}\x1b[0m"
+    return f"{ANSI_GREEN}{string}{ANSI_RESET}"
 
 def yellow(string):
-    return f"\x1b[33m{string}\x1b[0m"
+    return f"{ANSI_YELLOW}{string}{ANSI_RESET}"
 
 def blue(string):
-    return f"\x1b[34m{string}\x1b[0m"
+    return f"{ANSI_BLUE}{string}{ANSI_RESET}"
 
 def magenta(string):
-    return f"\x1b[35m{string}\x1b[0m"
+    return f"{ANSI_MAGENTA}{string}{ANSI_RESET}"
 
 def cyan(string):
-    return f"\x1b[36m{string}\x1b[0m"
+    return f"{ANSI_CYAN}{string}{ANSI_RESET}"
 
 def gray(string):
-    return f"\x1b[37m{string}\x1b[0m"
+    return f"{ANSI_GRAY}{string}{ANSI_RESET}"
