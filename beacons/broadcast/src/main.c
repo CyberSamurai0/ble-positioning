a479a461750a943053565814aae0a72550a5f638
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-11-27 00:31:53 -0500

    Add some basic test code

--- beacons/broadcast/src/main.c
+++ beacons/broadcast/src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 
+//static const POSITION[] = {1,5}; // Sample XY Coordinate System
+
 int main(void) {
+    int error = bt_enable(NULL);
+
+    if (error) {
+        printk("Bluetooth init failed (err %d)\n", error);
+        return error;
+    }
+
+    printk("Bluetooth initialized\n");
+
     return 0;
 }
