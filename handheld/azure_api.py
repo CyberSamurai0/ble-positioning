38ee141a9def407a90397931ca92d24b583d731c
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-11-29 23:19:59 -0500

    Use the position class rather than manual json

--- handheld/azure_api.py
+++ handheld/azure_api.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import position
 
 reg_endpoint = os.getenv("AZURE_REGISTER_URL")
 patch_endpoint = os.getenv("AZURE_UPDATE_URL")
@@ -9,21 +10,21 @@ def register():
     pass
 
 # Function to update the latest telemetry of the current device on Azure IoT Hub
-def push(x, y, building_id=None, floor=None):
+def push(pos):
+    if type(pos) is not position.Position:
+        return
+
     # Set Headers
     headers = {
         "Content-Type": "application/json",
+        # TODO authorization token potentially retrieved from register endpoint
         "Authorization": f"Bearer {os.getenv('AZURE_AUTH_TOKEN')}", # Load this to memory only when we need it
         "X-CNIT-546": True,
     }
 
     # Build Body
-    body = {
-        "loc_north": x,
-        "loc_east": y,
-        "building": building_id,
-        "floor": floor
-    }
+    body = pos.dict()
+    # TODO additional fields as needed
 
     response = requests.post(patch_endpoint, json=body, headers=headers)
     return response.status_code
