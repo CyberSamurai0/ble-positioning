82dbb9ced32e8eb13cac575040fc2365d9d6f0f8
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-12-01 16:50:20 -0500

    Update sensors.py

--- handheld/sensors.py
+++ handheld/sensors.py
@@ -1,9 +1,22 @@
+import numbers
+
 from position import Position
 import time
 
-class Sensors:
-    def __init__(self):
+class SensorCache:
+    def __init__(self, expiry_time):
         self.cache = {}
+
+        if not isinstance(expiry_time, numbers.Number):
+            expiry_time = 0
+
+        # Configure the time in seconds to wait before removing a beacon from the cache.
+        # This time is used to gauge whether a beacon is no longer in range or has fallen
+        # offline. Currently, every beacon should transmit at least once per second.
+        # However, we may sometimes lose incoming advertisements. Adjust as needed.
+        # If set to 0, do not remove old sensors from the cache. This will make best-fit
+        # selection more costly.
+        self.expiry_time = expiry_time
 
     def record_sensor(self, pos, rssi):
         if type(pos) is not Position:
@@ -18,8 +31,8 @@ class Sensors:
         # Avoid concurrent modification
         to_remove = []
         for pos, val in self.cache.items():
-            # If we haven't heard from a beacon in 5s
-            if val.time < time.time() - 5:
+            # If we haven't heard from a beacon in the expiry time
+            if self.expiry_time != 0 and val.time < time.time() - self.expiry_time:
                 to_remove.append(pos)
 
         for pos in to_remove:
@@ -29,6 +42,8 @@ class Sensors:
     def get_best_sensors(self):
         # Prioritize recency, then proximity
         # Return three cache objects (position and RSSI)
+
+        #
         return None, None, None
 
     def trilaterate(self):
