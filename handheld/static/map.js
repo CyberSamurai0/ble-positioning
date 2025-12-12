dcc060d4e30cbfe79aec990f14bec8fabdd644e1
CyberSamurai0 <colinsaumure@gmail.com>
2025-12-12 00:06:00 -0500

    Code cleanup and comments

--- handheld/static/map.js
+++ handheld/static/map.js
@@ -1,3 +1,5 @@
+console.log("Created for Purdue's CNIT54600 Course.\nSee https://github.com/CyberSamurai0/ble-positioning for more information.")
+
 const MIN_ZOOM = -3;
 const MAX_ZOOM = 0;
 const DISABLE_ZOOM = false;
@@ -5,6 +7,9 @@ const SHOW_BEACONS = true; // TODO disable beacon markers in release
 
 import {metersToPixels, pixelsToMeters, getImageDimensions} from "./helpers.js"
 
+/***** MAP SETUP *****/
+
+// Create a Leaflet map instance within the element with id "map"
 let map = L.map("map", {
     crs: L.CRS.Simple,
     minZoom: MIN_ZOOM,
@@ -12,14 +17,22 @@ let map = L.map("map", {
     zoomControl: !DISABLE_ZOOM,
 });
 
+// Use mouse movement events to track and display the mouse's live position
 map.on('mousemove', e => {
     const coordinateBox = document.getElementById('coordinate-box');
-    const lat = e.latlng.lat.toFixed(2); // Limit to 4 decimal places
+
+    // Limit to 2 decimal places
+    const lat = e.latlng.lat.toFixed(2);
     const lng = e.latlng.lng.toFixed(2);
 
+    // Show units in meters as well
     const m_north = pixelsToMeters(e.latlng.lat).toFixed(2);
     const m_east = pixelsToMeters(e.latlng.lng).toFixed(2);
-    coordinateBox.textContent = `Coordinates: (${lat}, ${lng})\r\nFeet: (${(lat/30).toFixed(2)}, ${(lng/30).toFixed(2)})\r\nMeters: ${m_north}, ${m_east}`;
+
+    // Set the text box content with all three values
+    coordinateBox.textContent = `Coordinates: (${lat}, ${lng})\r\n` +
+        `Feet: (${(e.latlng.lat/30).toFixed(2)}, ${(e.latlng.lng/30).toFixed(2)})\r\n` +
+        `Meters: (${m_north}, ${m_east})`;
 });
 
 getImageDimensions("./floorplans/0001-04.png").then(dimensions => {
@@ -111,9 +124,7 @@ if (SHOW_BEACONS) {
     beaconsHTTP.onreadystatechange = () => {
         if (beaconsHTTP.readyState === 4 && beaconsHTTP.status === 200) {
             beaconData = JSON.parse(beaconsHTTP.responseText);
-            console.log(beaconData);
             updateBeacons();
-            console.log(beaconMarkers);
         }
     }
 
